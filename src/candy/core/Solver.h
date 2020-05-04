528945e4325850909e9649e69b6e6201ce562be3
Markus Iser <markus.iser@kit.edu>
2020-05-04 18:40:59 +0200

    simplified schema of static polymorphism in candy

--- src/candy/core/Solver.h
+++ src/candy/core/Solver.h
@@ -84,7 +84,7 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 
 namespace Candy {
 
-template<class TClauses = ClauseDatabase, class TAssignment = Trail, class TPropagate = Propagate, class TLearning = ConflictAnalysis, class TBranching = VSIDS> 
+template<class TPropagate = Propagate, class TLearning = ConflictAnalysis, class TBranching = VSIDS> 
 class Solver : public CandySolverInterface {
 public:
     Solver();
@@ -154,8 +154,8 @@ public:
     }
 
 protected:
-    TClauses clause_db;
-    TAssignment trail;
+    ClauseDatabase clause_db;
+    Trail trail;
     TPropagate propagator;
     TLearning conflict_analysis;
     TBranching branch;
@@ -282,8 +282,8 @@ private:
 
 };
 
-template<class TClauses, class TAssignment, class TPropagate, class TLearning, class TBranching>
-Solver<TClauses, TAssignment, TPropagate, TLearning, TBranching>::Solver() : 
+template<class TPropagate, class TLearning, class TBranching>
+Solver<TPropagate, TLearning, TBranching>::Solver() : 
     // Basic Systems
     clause_db(),
     trail(),
@@ -311,20 +311,20 @@ Solver<TClauses, TAssignment, TPropagate, TLearning, TBranching>::Solver() :
     learntCallbackState(nullptr), learntCallbackMaxLength(0), learntCallback(nullptr)
 { }
 
-template<class TClauses, class TAssignment, class TPropagate, class TLearning, class TBranching>
-Solver<TClauses, TAssignment, TPropagate, TLearning, TBranching>::~Solver() {
+template<class TPropagate, class TLearning, class TBranching>
+Solver<TPropagate, TLearning, TBranching>::~Solver() {
 }
 
-template<class TClauses, class TAssignment, class TPropagate, class TLearning, class TBranching>
-void Solver<TClauses, TAssignment, TPropagate, TLearning, TBranching>::clear() {
+template<class TPropagate, class TLearning, class TBranching>
+void Solver<TPropagate, TLearning, TBranching>::clear() {
     clause_db.clear();
     trail.clear();
     propagator.clear();
     branch.clear();
 }
 
-template<class TClauses, class TAssignment, class TPropagate, class TLearning, class TBranching>
-void Solver<TClauses, TAssignment, TPropagate, TLearning, TBranching>::init(const CNFProblem& problem, ClauseAllocator* allocator, bool lemma) {
+template<class TPropagate, class TLearning, class TBranching>
+void Solver<TPropagate, TLearning, TBranching>::init(const CNFProblem& problem, ClauseAllocator* allocator, bool lemma) {
     assert(trail.decisionLevel() == 0);
 
     // always initialize clause_db _first_
@@ -347,8 +347,8 @@ void Solver<TClauses, TAssignment, TPropagate, TLearning, TBranching>::init(cons
 }
 
 
-template<class TClauses, class TAssignment, class TPropagate, class TLearning, class TBranching>
-lbool Solver<TClauses, TAssignment, TPropagate, TLearning, TBranching>::search() {
+template<class TPropagate, class TLearning, class TBranching>
+lbool Solver<TPropagate, TLearning, TBranching>::search() {
     assert(!clause_db.hasEmptyClause());
 
     sonification.send("/restart", 1);
@@ -420,8 +420,8 @@ lbool Solver<TClauses, TAssignment, TPropagate, TLearning, TBranching>::search()
     return l_Undef; // not reached
 }
 
-template<class TClauses, class TAssignment, class TPropagate, class TLearning, class TBranching>
-lbool Solver<TClauses, TAssignment, TPropagate, TLearning, TBranching>::solve() {
+template<class TPropagate, class TLearning, class TBranching>
+lbool Solver<TPropagate, TLearning, TBranching>::solve() {
     sonification.send("/start", 1);
     sonification.send("/variables", nVars());
     sonification.send("/clauses", nClauses());
@@ -501,8 +501,8 @@ lbool Solver<TClauses, TAssignment, TPropagate, TLearning, TBranching>::solve() 
     return status;
 }
 
-template<class TClauses, class TAssignment, class TPropagate, class TLearning, class TBranching>
-void Solver<TClauses, TAssignment, TPropagate, TLearning, TBranching>::printStats() {
+template<class TPropagate, class TLearning, class TBranching>
+void Solver<TPropagate, TLearning, TBranching>::printStats() {
     std::cout << "c ************************* " << std::endl << std::left;
     std::cout << "c " << std::setw(20) << "restarts:" << nRestarts() << std::endl;
     std::cout << "c " << std::setw(20) << "conflicts:" << nConflicts() << std::endl;
